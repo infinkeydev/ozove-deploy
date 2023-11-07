a08ae89258921d49503202a014eecefaed3cf87d
tanveer anjum <tanveerdar933@gmail.com>
2023-11-07 15:57:59 +0500

    added sitemap.xml and robots.txt

--- next.config.js
+++ next.config.js
@@ -1,39 +1,4 @@
-const fs = require('fs');
-const path = require('path');
 /** @type {import('next').NextConfig} */
-
-const copyFiles = () => {
-  try {
-    const sourceDirectory = __dirname; // Root directory
-    const destinationDirectory = path.join(__dirname, '.next'); // .next directory
-
-    const filesToCopy = ['robots.txt', 'sitemap.xml'];
-
-    const copyFile = (file) => {
-      const sourceFile = path.join(sourceDirectory, file);
-      const destinationFile = path.join(destinationDirectory, file);
-
-      if (fs.existsSync(sourceFile)) {
-        fs.copyFileSync(sourceFile, destinationFile);
-        console.log(`${file} was copied to .next directory`);
-      } else {
-        console.error(`File '${file}' does not exist in the root directory`);
-      }
-    };
-
-    filesToCopy.forEach((file) => {
-      copyFile(file);
-    });
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-};
-
-
-if (process.env.NODE_ENV === 'production') {
-  copyFiles();
-}
-
 
 const nextConfig = {
   reactStrictMode: true,
@@ -44,10 +9,7 @@ const nextConfig = {
     });
 
     return config;
-  },
-  async redirects() {
-    return [];
-  },
+  }
 };
 
 module.exports = nextConfig;
